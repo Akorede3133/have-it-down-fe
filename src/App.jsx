f9e15261ab425238444efa170e46d59335f3440f
akorede <emiolasaheed1@gmail.com>
2024-01-27 17:06:29 +0100

    Fix route errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import { createRoutesFromElements, createBrowserRouter, RouterProvider, Route } from 'react-router-dom'
 import AppLayout from './components/AppLayout'
 import Feeds from './features/feeds/routes/Feeds'
+import FollowingFeeds from './features/feeds/routes/FollowingFeeds'
+
 import FeedsLayout from './components/FeedsLayout'
-import FollowingFeeds from './features/feeds/FollowingFeeds'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import Settings from './features/settings/routes/Settings'
 import Login from './features/auth/routes/Login'
 import Register from './features/auth/routes/Register'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { Toaster } from 'react-hot-toast'
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -30,6 +33,9 @@ const App = () => {
   ))
   return (
     <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+      <Toaster 
+      />
       <RouterProvider router={router} />
     </QueryClientProvider>
   )
