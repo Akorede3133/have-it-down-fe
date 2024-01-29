f0085b9b5285aae84b07540e4e390fa6852bada0
akorede <emiolasaheed1@gmail.com>
2024-01-29 21:23:37 +0100

    Implement user login

--- src/features/auth/components/LoginForm.jsx
+++ src/features/auth/components/LoginForm.jsx
@@ -3,11 +3,13 @@ import Input from "./Input";
 import { useForm } from "react-hook-form";
 import PasswordInput from "./PasswordInput";
 import { Link } from "react-router-dom";
+import useLogin from "../hooks/useLogin";
 
 const LoginForm = () => {
   const { register, handleSubmit, formState: { errors } } = useForm();
+  const { loginUser, isLoggingIn } = useLogin();
   const onSubmit = (data) => {
-    console.log(data);
+    loginUser(data);
   }
   return (
     <form onSubmit={handleSubmit(onSubmit)} className="w-[80%] mx-auto md:w-[450px]">
@@ -15,7 +17,7 @@ const LoginForm = () => {
         <Input icon={<HiOutlineEnvelope />} type='email' placeholder='Email' register={register} name="email" errors={errors} />
         <PasswordInput register={register} errors={errors} />
       </section>
-      <button className="bg-black text-white rounded-full px-10 py-3 mx-auto block mt-6">Login</button>
+      <button className="bg-black text-white rounded-full px-10 py-3 mx-auto block mt-6" disabled={isLoggingIn}>{isLoggingIn ? 'Logging in' : 'Login'}</button>
       <p className="mt-10 text-center">Don&apos;t have an account? <Link to='/register' className=" underline">Join us today</Link></p>
     </form>
   )
