067c28a57dc759bb292523e40ef9be0198c2a4d3
akorede <emiolasaheed1@gmail.com>
2024-01-29 21:09:08 +0100

    Create login APi

---
+++ src/features/auth/api/loginApi.js
@@ -0,0 +1,21 @@
+const BASE_URL = import.meta.env.VITE_API_BASE_URL;
+const login = async (data) => {
+  try {
+    const response = await fetch(`${BASE_URL}/login`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    });
+    const result = await response.json();
+    if (!response.ok) {
+      throw new Error(result.message);
+    }
+    return result;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+}
+
+export default login;
