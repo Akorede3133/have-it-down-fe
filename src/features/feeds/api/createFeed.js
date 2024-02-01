c37bf917cc9ff7657c5fbd7664fd9cea229bc404
akorede <emiolasaheed1@gmail.com>
2024-02-01 17:39:27 +0100

    Create api to create post

---
+++ src/features/feeds/api/createFeed.js
@@ -0,0 +1,22 @@
+const BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
+const createFeed = async (data) => {
+  try {
+    const response = await fetch(`${BASE_URL}/feeds`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    })
+    const result = await response.json();
+    if (!response.ok) {
+      throw new Error(result.message);
+    }
+    console.log(result);
+    return result;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+}
+export default createFeed;
