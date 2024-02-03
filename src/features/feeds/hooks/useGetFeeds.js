d086212683da04b83d20743631bf0c79b24887dd
akorede <emiolasaheed1@gmail.com>
2024-02-03 15:53:31 +0100

    Create hook to get feeds

---
+++ src/features/feeds/hooks/useGetFeeds.js
@@ -0,0 +1,13 @@
+import { useQuery } from "@tanstack/react-query"
+import getFeeds from "../api/getFeeds"
+
+const useGetFeeds = () => {
+  const { data: feeds, isPending: isGettingFeeds, error } = useQuery({
+    queryFn: () => getFeeds,
+    queryKey: ['feeds']
+  })
+
+  return { feeds, isGettingFeeds, error };
+}
+
+export default useGetFeeds;
