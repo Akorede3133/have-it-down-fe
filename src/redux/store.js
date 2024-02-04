09d5917831dd85a47ba2422ebdc393e5124d362b
akorede <emiolasaheed1@gmail.com>
2024-02-04 22:11:19 +0100

    Create user slice

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import editorReducer from './EditorSlice';
+import userReducer from './userSlice';
 
 const store = configureStore({
   reducer: {
     editor: editorReducer,
+    user: userReducer
   }
 })
 
