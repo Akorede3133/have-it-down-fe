f88dc6fe531473f292a5dc1daa13b72003d18645
akorede <emiolasaheed1@gmail.com>
2024-02-06 22:21:35 +0100

    Create api to create reply

---
+++ src/features/comment/api/createReply.js
@@ -0,0 +1,26 @@
+const BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
+const createReply = async (parentId, content) => {
+  console.log(parentId, content);
+  try {
+    const response = await fetch(`${BASE_URL}/comments/${parentId}`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ content}),
+      credentials: "include",
+    })
+    const result = await response.json();
+    if (!response.ok) {
+      console.log(result);
+      throw new Error(result.message);
+    }
+    console.log(result);
+    return result;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+}
+
+export default createReply;
