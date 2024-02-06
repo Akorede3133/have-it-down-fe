b7884cbb0b1becf2e6ebee13b983632e64bdd6e3
akorede <emiolasaheed1@gmail.com>
2024-02-06 17:03:41 +0100

    Create api for creating comment

---
+++ src/features/comment/api/createComment.js
@@ -0,0 +1,25 @@
+const BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
+const createComment = async (feedId, parentId, content) => {
+  try {
+    const response = await fetch(`${BASE_URL}/comments/${feedId}/${parentId}`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(content),
+      credentials: 'include',
+    })
+    const result = await response.json();
+
+    if (!response.ok) {
+      throw new Error(result.message);
+    }
+    console.log(result);
+    return result;
+  } catch (error) {
+    throw new Error(error.message)
+  }
+}
+
+export default createComment;
