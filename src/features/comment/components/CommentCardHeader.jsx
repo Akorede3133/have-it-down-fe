8f5b8acb3bcba13b6e9c173b3cf8c2d029b6d03c
akorede <emiolasaheed1@gmail.com>
2024-02-08 09:12:47 +0100

    Display author tag if author is also the commenter

--- src/features/comment/components/CommentCardHeader.jsx
+++ src/features/comment/components/CommentCardHeader.jsx
@@ -1,15 +1,17 @@
 import { HiOutlineEllipsisHorizontal } from "react-icons/hi2"
 import ProfileAvatar from "../../../components/ProfileAvatar"
 import { formatDateInDistance } from "../../../utils/date"
+import useGetCurrentUser from "../../auth/hooks/useGetCurrentUser"
 
 const CommentCardHeader = ({ name , createdAt}) => {
+  const { user } = useGetCurrentUser();
   return (
     <div>
         <section className="flex justify-between items-center">
         <div className="flex items-center gap-2 my-4">
           <ProfileAvatar />
           <div className="">
-            <p className=" font-medium">{name}</p>
+            <p className=" font-medium flex items-center gap-2"><span>{name}</span>{  user.name === name && <span className="bg-[#1a8917] uppercase text-[10px] text-white px-1">Author</span>}</p>
             <p className="text-sm">{ formatDateInDistance(createdAt)}</p>
           </div>
         </div>
